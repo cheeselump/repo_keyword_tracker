e926bf6c5901876b95c676cb1e12105837907652
HMX_BrandonChoo <cheeselump@gmail.com>
2020-11-13 17:51:02 +0800

    Added Folder Selection Feature
    Just started the project.
    
    Step 1: Define the path to search for our desired keyword.

---
+++ HMX_Code_Tracker/HMX_Code_Tracker/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace HMX_Code_Tracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_SelectDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txt_Directory.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
